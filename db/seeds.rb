92b14da55054ba8f02452e1a52b92f97ec8f9dc5
Peter Lynchgit logcleargit config --global user.name Peter <peterlynch101@gmail.com>
2018-11-25 22:12:36 +0000

    integrated show functionality for the portfolio module

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,8 @@ puts "5 skills created"
      title: "Portfolio title: #{portfolio_item}",
      subtitle: "My great service",
      body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat",
-     main_image: "https://via.placehold.it/600x400",
-     thumb_image: "https://via.placehold.it/350x200"
+     main_image: "https://via.placeholder.com/600",
+     thumb_image: "https://via.placeholder.com/350"
     )
 end
 
