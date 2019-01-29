6c9c4ad527b33c8b1f40827b2daa98ccf85ce1f1
Peter Lynchgit logcleargit config --global user.name Peter <peterlynch101@gmail.com>
2019-01-29 20:11:01 +0000

    implemented inital design for portfolio index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ puts "5 skills created"
      subtitle: "Ruby on Rails",
      body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat",
      main_image: "https://via.placeholder.com/600",
-     thumb_image: "https://via.placeholder.com/350"
+     thumb_image: "https://via.placeholder.com/350x200"
     )
 end
 
@@ -38,7 +38,7 @@ end
      subtitle: "Angular",
      body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat",
      main_image: "https://via.placeholder.com/600",
-     thumb_image: "https://via.placeholder.com/350"
+     thumb_image: "https://via.placeholder.com/350x200"
     )
 end
 
