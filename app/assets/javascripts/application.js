d01df31b9f46421f7c83221e7c085c0a612f8851
Peter Lynchgit logcleargit config --global user.name Peter <peterlynch101@gmail.com>
2019-04-01 16:49:48 +0100

    updated profile image for about me page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require jquery3
-//= require bootstrap-sprockets
+
 //= require jquery_ujs
 //= require jquery-ui
 //= require html.sortable
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
 //= require popper
 
